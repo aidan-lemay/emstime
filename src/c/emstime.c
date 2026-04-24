b9540b0196e78463a89577abe126a2e7827dd479
Aidan LeMay <aquader14@gmail.com>
2026-04-24 11:30:43 -0400

    Adding basic framework for a menu with more options

--- src/c/emstime.c
+++ src/c/emstime.c
@@ -1,10 +1,54 @@
 #include <pebble.h>
 
-static Window *s_window;
+static Window *s_window, *s_menu_window;
 static TextLayer *s_layer_24h, *s_layer_dateLong, *s_layer_dateShort, *s_layer_12h;
+static MenuLayer *s_menu_layer;
+
+static GColor s_bg_color = GColorBlack;
+static GColor s_fg_color = GColorWhite;
 
 // Buffer to hold our formatted time/date strings
 static char s_time_buffer[64];
+
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *callback_context) {
+    return 4; // Number of items in your menu
+}
+
+static void menu_draw_row_callback(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *callback_context) {
+    switch (cell_index->row) {
+        case 0: menu_cell_basic_draw(ctx, cell_layer, "15 Minute", "15 Minute Reminder", NULL); break;
+        case 1: menu_cell_basic_draw(ctx, cell_layer, "5 Minute", "5 Minute Reminder", NULL); break;
+        case 2: menu_cell_basic_draw(ctx, cell_layer, "Stop Timers", "Stop All Timers", NULL); break;
+        case 3: menu_cell_basic_draw(ctx, cell_layer, "Exit", NULL, NULL); break;
+    }
+}
+
+static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
+    if (cell_index->row == 3) {
+        window_stack_pop(true); // Close the menu
+    }
+}
+
+static void menu_window_load(Window *window) {
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+
+    s_menu_layer = menu_layer_create(bounds);
+    menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks) {
+        .get_num_rows = menu_get_num_rows_callback,
+        .draw_row = menu_draw_row_callback,
+        .select_click = menu_select_callback,
+    });
+    
+    // Crucial: Set the click config provider for the menu
+    menu_layer_set_click_config_onto_window(s_menu_layer, window);
+    
+    layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+}
+
+static void menu_window_unload(Window *window) {
+    menu_layer_destroy(s_menu_layer);
+}
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -28,6 +72,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void prv_window_load(Window *window) {
+  window_set_background_color(s_window, s_bg_color);
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
@@ -50,21 +95,29 @@ static void prv_window_load(Window *window) {
   text_layer_set_text_alignment(layers[0], GTextAlignmentCenter);
   text_layer_set_font(layers[0], fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(layers[0]));
+  text_layer_set_background_color(layers[0], GColorClear);
+  text_layer_set_text_color(layers[0], s_fg_color);
 
   // Long Date
   text_layer_set_text_alignment(layers[1], GTextAlignmentCenter);
   text_layer_set_font(layers[1], fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(layers[1]));
+  text_layer_set_background_color(layers[1], GColorClear);
+  text_layer_set_text_color(layers[1], s_fg_color);
 
   // Short Date
   text_layer_set_text_alignment(layers[2], GTextAlignmentCenter);
   text_layer_set_font(layers[2], fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(layers[2]));
+  text_layer_set_background_color(layers[2], GColorClear);
+  text_layer_set_text_color(layers[2], s_fg_color);
 
   // 12hr Time
   text_layer_set_text_alignment(layers[3], GTextAlignmentCenter);
   text_layer_set_font(layers[3], fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(layers[3]));
+  text_layer_set_background_color(layers[3], GColorClear);
+  text_layer_set_text_color(layers[3], s_fg_color);
 
   update_time();
 }
@@ -76,8 +129,29 @@ static void prv_window_unload(Window *window) {
   text_layer_destroy(s_layer_12h);
 }
 
+static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  // Initialize the menu window if not already done
+  if(!s_menu_window) {
+    s_menu_window = window_create();
+    window_set_window_handlers(s_menu_window, (WindowHandlers) {
+      .load = menu_window_load,
+      .unload = menu_window_unload,
+    });
+  }
+  
+  // Push the menu window onto the stack
+  window_stack_push(s_menu_window, true);
+}
+
+static void prv_click_config_provider(void *context) {
+  // Bind the SELECT button to your handler
+  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
+}
+
 static void prv_init(void) {
   s_window = window_create();
+
+  window_set_click_config_provider(s_window, prv_click_config_provider);
 
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
